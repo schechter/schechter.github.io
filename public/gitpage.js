5c849cfced3b3ac6e54aca919a50d24b79fed720
Mike Schechter <schechtermike@gmail.com>
2013-08-15 14:45:25 -0400

    add project euler friend key

--- public/gitpage.js
+++ public/gitpage.js
@@ -17,13 +17,29 @@ function hide_number(){
   $('.my_number').hide();
 }
 
+
+function show_uelerkey(){
+  if($('.my-euler-key').size() === 0) {
+    var key_div = $('<div>');
+    key_div.addClass('my_euler-key');
+    key_div.text('My Friend Key' + '\n' +'35073569505731_fe2fcbb8a2bbfee7c9e8e3def448a70b');
+    $('#social-links').append(key_div);
+  }
+}
+
+function hide_uelerkey(){
+  setTimeout(function(){
+  $('.my_euler-key').remove();
+  },500);
+}
+
 function show_tech (e){
   var tech_text = e.target.id;
   $('#logo-show').text(tech_text);
 }
 
 function hide_tech (){
-   $('#logo-show').text('')
+   $('#logo-show').text('');
 }
 
 $(function(){
@@ -31,6 +47,8 @@ $(function(){
   $('.section1-text').on('mouseout', '#title-page-description', remove_aspiring);
   $('#social-links').on('hover', '.phone-icon', show_number);
   $('#social-links').on('mouseout', '.phone-icon', hide_number);
+  $('#social-links').on('mouseenter', '.projecteuler-icon', show_uelerkey);
+  $('#social-links').on('mouseout', '.projecteuler-icon', hide_uelerkey);
   $('#techs').on('hover', 'img', show_tech);
   $('#techs').on('mouseout', 'img', hide_tech);  
 });
